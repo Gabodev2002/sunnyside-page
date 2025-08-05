70a669ae979ae596e90cf9e040aaa24d9f6d2d55
Gabo Gonzalez <gabo.mgb6@gmail.com>
2025-08-05 07:46:39 -0700

    add new header content animation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const menuColapse = document.getElementById('menu-colapse')
 const btnMenu = document.getElementById('btn-menu')
+const divContent = document.getElementsByClassName('header-content')
 
 btnMenu.addEventListener('click', () => {
     menuColapse.classList.toggle('colapse')
@@ -12,9 +13,11 @@ function scrollFunction() {
   const nav = document.getElementById("nav"); 
 
   if (window.scrollY > 0) { 
-    nav.classList.add('scrolled');  
+    nav.classList.add('scrolled'); 
+    divContent[0].classList.add('scrolled-content'); 
   } else {
     nav.classList.remove('scrolled');
+    divContent[0].classList.remove('scrolled-content')
   }
 }
 window.addEventListener('scroll', scrollFunction); 
